c2500fe2b4602f6a7867d196a0e437b3e2ad348f
Fudong Bai <fudongbai@gmail.com>
2015-05-10 11:23:31 +0800

    package: add post script for adbd
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

--- package/adbd/adbd.mk
+++ package/adbd/adbd.mk
@@ -10,4 +10,8 @@ ADBD_SITE = ftp://192.168.1.101/adbd/
 ADBD_LICENSE = GPLv2+
 ADBD_LICENSE_FILES = COPYING
 
+define ADBD_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 package/adbd/S88adbd $(TARGET_DIR)/etc/init.d/S88adbd
+endef
+
 $(eval $(autotools-package))
